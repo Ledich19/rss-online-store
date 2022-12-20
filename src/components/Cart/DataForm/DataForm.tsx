bff0f27919e3ce4e82842db44af5e6dc1cf146b2
Aleksander Chumachenko <ledich19@gmail.com>
2022-12-20 21:06:33 +0200

    feat: add validate

--- src/components/Cart/DataForm/DataForm.tsx
+++ src/components/Cart/DataForm/DataForm.tsx
@@ -5,11 +5,27 @@ import mastercard from '../../../images/pay-siatem/mastercard.png'
 import maestro from '../../../images/pay-siatem/maestro.png'
 import jsb from '../../../images/pay-siatem/jcb.png'
 
-
 const DataForm = () => {
+  const [isDirty, setIsDirty] = useState(false)
+
+  const [name, setName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [postCode, setPostCode] = useState('')
+  const [address, setAddress] = useState('')
+
+  const [nameError, setNameError] = useState('')
+  const [phoneNumberError, setPhoneNumberError] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [addressError, setAddressError] = useState('')
+
   const [cardNum, setCardNum] = useState('')
   const [cardValid, setCardValid] = useState('')
   const [cardCvv, setCardCvv] = useState('')
+
+  const [cardNumError, setCardNumError] = useState('')
+  const [cardValidError, setCardValidError] = useState('')
+  const [cardCvvError, setCardCvvError] = useState('')
 
   let card = ''
   if (cardNum[0] <= '2') {
@@ -22,11 +38,110 @@ const DataForm = () => {
     card = jsb
   }
 
+  const nameValidate = (name: string) => {
+    const nameArr = name.split(' ')
+    if (name === undefined) {
+      setNameError('must be filled')
+      return false
+    } else if (nameArr.length < 2) {
+      setNameError('must have at least two words')
+      return false
+    } else if (!nameArr.every((name) => name.length > 2)) {
+      setNameError('must be longer than three characters')
+      return false
+    }
+    setNameError('')
+    return true
+  }
+  const emailValidate = (email: string) => {
+    const re =
+      /^[a-zA-Z0-9.!#$%&'*+=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+
+    if (!re.test(email) || email === undefined) {
+      setEmailError('incorrect email')
+      return false
+    }
+    setEmailError('')
+    return true
+  }
+  const numberValidate = (phoneNumber: string) => {
+    if (phoneNumber === undefined || phoneNumber.length === 0) {
+      setPhoneNumberError('must be filled')
+      return false
+    } else if (phoneNumber[0] !== '+') {
+      setPhoneNumberError('must start with +')
+      return false
+    } else if (phoneNumber.length < 10) {
+      setPhoneNumberError('must be at least 9 characters')
+      return false
+    }
+    setPhoneNumberError('')
+    return true
+  }
+  const addressValidate = (address: string) => {
+    const addressArr = address.split(' ')
+    if (address === undefined || address.length === 0) {
+      setAddressError('must be filled')
+      return false
+    } else if (addressArr.length < 3) {
+      setAddressError('must have at least three words')
+      return false
+    } else if (!addressArr.every((word) => word.length > 4)) {
+      setAddressError('length of each is at least 5 characters')
+      return false
+    }
+    setAddressError('')
+    return true
+  }
+  const cardNumberValidate = (cardNum: string) => {
+    console.log(cardNum)
+
+    if (cardNum === undefined || cardNum.length === 0) {
+      setCardNumError('must be filled')
+      console.log(11111)
+
+      return false
+    } else if (cardNum.length < 16) {
+      console.log(22222)
+
+      setCardNumError('incorrect card number')
+      return false
+    }
+    setCardNumError('')
+    return true
+  }
+  const cardValidValidate = (cardValid: string) => {
+    const cardValidArr = cardValid.split('/')
+    if (cardValid === undefined || cardValid.length === 0) {
+      setCardValidError('must be filled')
+      return false
+    } else if (parseInt(cardValidArr[0]) > 12) {
+      setCardValidError('month cannot be more than 12')
+      return false
+    }
+    setCardValidError('')
+    return true
+  }
+  const cardCvvValidate = (cardCvv: string) => {
+    if (cardCvv === undefined || cardCvv.length === 0) {
+      setCardCvvError('must be filled')
+      return false
+    } else if (cardCvv.length < 3) {
+      setCardCvvError('must have at least three words')
+      return false
+    }
+    setCardCvvError('')
+    return true
+  }
+
   const handleCartNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault()
     const value = e.target.value
     if (value.length <= 16) {
       setCardNum(value)
+    }
+    if (isDirty) {
+      cardNumberValidate(value)
     }
   }
   const handleCartValid = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,27 +157,138 @@ const DataForm = () => {
       }
       setCardValid(value)
     }
+    if (isDirty) {
+      cardValidValidate(value)
+    }
   }
   const handleCartCvv = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
     const value = e.target.value
     if (value.length <= 3) {
       setCardCvv(value)
     }
+    if (isDirty) {
+      cardCvvValidate(value)
+    }
+  }
+  const nameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const value = e.target.value
+    setName(value)
+    if (isDirty) {
+      nameValidate(value)
+    }
+  }
+  const phoneNumberHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const value = e.target.value
+    if (
+      !Number.isNaN(parseInt(value[value.length - 1], 10)) ||
+      value[value.length - 1] === '+' ||
+      value[value.length - 1] === undefined
+    ) {
+      setPhoneNumber(value)
+    }
+    if (isDirty) {
+      numberValidate(value)
+    }
+  }
+  const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const value = e.target.value
+    setEmail(value)
+    if (isDirty) {
+      emailValidate(value)
+    }
+  }
+  const postCodeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const value = e.target.value
+    setPostCode(value)
+  }
+  const addressHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const value = e.target.value
+    setAddress(value)
+    if (isDirty) {
+      addressValidate(value)
+    }
   }
 
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    setIsDirty(true)
+    e.preventDefault()
+    nameValidate(phoneNumber)
+    emailValidate(email)
+    numberValidate(phoneNumber)
+    addressValidate(address)
+    cardNumberValidate(cardNum)
+    cardValidValidate(cardValid)
+    cardCvvValidate(cardCvv)
+
+    if (
+      !nameError ||
+      !phoneNumberError ||
+      !emailError ||
+      !addressError ||
+      !cardNumError ||
+      !cardValidError ||
+      !cardCvvError
+    ) {
+
+     
+    }
+  }
   return (
-    <form className="order">
+    <form onSubmit={handleSubmit} className="order">
       <div className="order__address">
-        <input className="order__address-item" type="text" placeholder="Name" />
-        <input className="order__address-item" type="text" placeholder="Surname" />
-        <input className="order__address-item" type="text" placeholder="Region" />
-        <input className="order__address-item" type="tel" placeholder="Phone Number" />
-        <input className="order__address-item" type="email" placeholder="Email" />
-        <input className="order__address-item" type="text" placeholder="Postcode" />
-        <input className="order__address-item" type="text" placeholder="City" />
-        <input className="order__address-item" type="text" placeholder="Address" />
+        {nameError && <div className="order__valid-error">{nameError}</div>}
+        <input
+          value={name}
+          onChange={nameHandler}
+          className="order__address-item order__address-item_wide"
+          type="text"
+          placeholder="Name"
+        />
+
+        {emailError && <div className="order__valid-error">{emailError}</div>}
+        <input
+          value={email}
+          onChange={emailHandler}
+          className="order__address-item order__address-item_wide"
+          type="email"
+          placeholder="Email"
+        />
+
+        {phoneNumberError && <div className="order__valid-error">{phoneNumberError}</div>}
+        <input
+          value={phoneNumber}
+          onChange={phoneNumberHandler}
+          className="order__address-item"
+          type="tel"
+          placeholder="Phone Number"
+        />
+
+        <input
+          value={postCode}
+          onChange={postCodeHandler}
+          className="order__address-item"
+          type="number"
+          placeholder="Postcode"
+        />
+
+        {addressError && <div className="order__valid-error">{addressError}</div>}
+        <input
+          value={address}
+          onChange={addressHandler}
+          className="order__address-item"
+          type="text"
+          placeholder="Address"
+        />
       </div>
+
       <div className="order__card card" style={{ backgroundImage: `url(${card})` }}>
+        {cardNumError && <div className="order__valid-error">{cardNumError}</div>}
         <input
           onChange={handleCartNumber}
           value={cardNum}
@@ -70,6 +296,8 @@ const DataForm = () => {
           className="card__number"
           placeholder="number"
         ></input>
+
+        {cardValidError && <div className="order__valid-error">{cardValidError}</div>}
 
         <div className="card__box">
           <input
@@ -80,8 +308,10 @@ const DataForm = () => {
             className="card__valid"
             placeholder="valid"
           ></input>
+
+          {cardCvvError && <div className="order__valid-error">{cardCvvError}</div>}
+
           <input
-          
             onChange={handleCartCvv}
             value={cardCvv}
             type="number"
@@ -90,7 +320,7 @@ const DataForm = () => {
           ></input>
         </div>
       </div>
-      <button className="card__order-btn">Order</button>
+      <button className="order__btn">Order</button>
     </form>
   )
 }
