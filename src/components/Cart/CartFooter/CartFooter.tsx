a8a64ba50336734f279f45f35f51a50f6414da83
Aleksander Chumachenko <ledich19@gmail.com>
2022-12-22 21:31:36 +0200

    feat: add back button

--- src/components/Cart/CartFooter/CartFooter.tsx
+++ src/components/Cart/CartFooter/CartFooter.tsx
@@ -11,24 +11,33 @@ const CartFooter = () => {
   const dispatch = useAppDispatch()
   const modalState = useAppSelector((state) => state.modals)
 
- 
-
   const handleOrderMenu = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault()
     dispatch(setIsOpenForm(true))
   }
+  const handleBeak = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    dispatch(setIsOpenForm(false))
+  }
 
   return (
     <div className="cart__footer">
-
-      
-
-
       {!modalState.isOderFinish && !modalState.isForm ? (
         <>
           <Promo />
           <div className="cart__button" onClick={handleOrderMenu}>
             GO TO THE SHOPPING MARKET
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
+
+      {!modalState.isOderFinish && modalState.isForm ? (
+        <>
+          <Promo />
+          <div className="cart__button" onClick={handleBeak}>
+            beack
           </div>
         </>
       ) : (
