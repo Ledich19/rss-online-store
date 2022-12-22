d0f8dab3e4e79a1d47d926994878e2eb2c92554d
pedanmax <m.pedan2017@gmail.com>
2022-12-22 21:53:02 +0200

    refactor:change text buttons in cart

--- src/components/Cart/CartFooter/CartFooter.tsx
+++ src/components/Cart/CartFooter/CartFooter.tsx
@@ -26,7 +26,7 @@ const CartFooter = () => {
         <>
           <Promo />
           <div className="cart__button" onClick={handleOrderMenu}>
-            GO TO THE SHOPPING MARKET
+            MAKE A PURCHASE
           </div>
         </>
       ) : (
@@ -37,7 +37,7 @@ const CartFooter = () => {
         <>
           <Promo />
           <div className="cart__button" onClick={handleBeak}>
-            beack
+            BACK TO CART
           </div>
         </>
       ) : (
