83ab6b4fb50fd48b8531e19ebbba5e76dcbfa9c0
pedanmax <m.pedan2017@gmail.com>
2022-12-26 18:31:56 +0200

    feat:add property rating to product-page

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -59,6 +59,10 @@ const ProductDetails = () => {
               <p className="item__text">{initialState[0].brand}</p>
             </div>
             <div className="item__prop">
+              <h5 className="item__label">Rate:</h5>
+              <p className="item__text">{initialState[0].rating}</p>
+            </div>
+            <div className="item__prop">
               <h5 className="item__label">Size:</h5>
               <select className="item__select"> 
                 <option className="item__size">S</option>
