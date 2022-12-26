2b5827495709fa5e7281926a6b4212e2363fd757
Aleksander Chumachenko <ledich19@gmail.com>
2022-12-26 23:16:41 +0200

    refactor: StoreFilters

--- src/components/Store/StoreFilters/StoreFilters.tsx
+++ src/components/Store/StoreFilters/StoreFilters.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useAppSelector } from '../../../app/hooks'
 import SortItem from './SortItem/SortItem'
 import SortRange from './SortRange/SortRange'
@@ -15,14 +14,15 @@ function unique<T>(arr: T[]): T[] {
 }
 
 const StoreFilters = () => {
-  const productsState = useAppSelector((state) => state.products)
-  const brands = unique(productsState.map((p) => p.brand))
-  const categorys = unique(productsState.map((p) => p.category))
+  const { products } = useAppSelector((state) => state)
+  
+  const brands = unique(products.map((p) => p.brand))
+  const categories = unique(products.map((p) => p.category))
 
   return (
     <div className="store__filters filters">
       <SortItem filters={['man', 'woman', 'kids']} title="sex" />
-      <SortItem filters={categorys} title="category" />
+      <SortItem filters={categories} title="category" />
       <SortItem filters={brands} title="brand" />
       <SortRange min={1} max={5000} step={1} title="price" />
       <SortRange min={1} max={5} step={0.1} title="rating" />
