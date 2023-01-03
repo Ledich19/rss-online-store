38a1a88d16540856455d9d37c5175331eb63ff20
pedanmax <m.pedan2017@gmail.com>
2023-01-03 12:46:45 +0200

    feat:add html css for input search

---
+++ src/components/Store/StoreFilters/SearchInput/Search.tsx
@@ -0,0 +1,12 @@
+import './Search.scss'
+
+
+const Search = () => {
+  return (
+    <div className="filtres__search search">
+      <input type="search" placeholder='Search...' className="search__input" />
+    </div>
+  )
+}
+
+export default Search
