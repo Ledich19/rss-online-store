7ccc2cca5c25f60d9d86caa69b41b01f03b46980
pedanmax <m.pedan2017@gmail.com>
2023-01-03 13:21:00 +0200

    feat:add copy url to clipboard

--- src/components/Store/StoreFilters/SearchInput/Search.tsx
+++ src/components/Store/StoreFilters/SearchInput/Search.tsx
@@ -1,10 +1,25 @@
+import React , {useState} from 'react';
 import './Search.scss'
+import { useClipboard } from 'use-clipboard-copy';
 
 
 const Search = () => {
+  const clipboard = useClipboard();
+  const [buttonState, setButtonState] = useState('Copy filtres');
+
+  const copy = () =>{
+    clipboard.copy(window.location.href);
+    setButtonState('Copied');
+    setTimeout(()=>setButtonState('Copy filtres'),1000)
+  }
+  
   return (
     <div className="filtres__search search">
-      <input type="search" placeholder='Search...' className="search__input" />
+      <div className="search__buttons">
+        <button className="search__button" onClick={copy}>{buttonState}</button>
+        <button className="search__button">Reset filtres</button>
+      </div>
+      <input type="search" placeholder='Search...'  className="search__input" />
     </div>
   )
 }
